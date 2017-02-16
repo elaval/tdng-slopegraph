7f6896a3df17de65aaac4b017c38d297a91c06c4
Ernesto Laval <Ernesto.Laval@EYC.COM>
2017-02-16 15:07:51 +0000

    Add module

---
+++ slopegraph.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SlopegraphComponent }   from './slopegraph.component';
+
+@NgModule({
+  imports: [],
+  exports: [SlopegraphComponent],
+  declarations: [SlopegraphComponent],
+  providers: [],
+})
+export class SlopegraphModule { }
